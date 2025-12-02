51763d7af58b338d412879634c1a6deb87ad6ab3
Pablo <pablogroza2000@gmail.com>
2025-12-03 00:49:31 +0100

    Agrego validacion del avatar para solo poder enviar jpg, jpeg y png evitando archivos maliciosos

--- signup-logic.php
+++ signup-logic.php
@@ -32,10 +32,38 @@ if(isset($_POST["submit"])) {
         } else {
             // Hasheamos las contraseñas
             $hashed_password = password_hash($createpassword, PASSWORD_DEFAULT);
-            echo $createpassword . "<br/>";
-            echo $hashed_password;
-        }
+
+            // Compruebo que el usuario y el correo electronico realmente existen ya en la base de datos
+            $user_check_query = "SELECT * FROM users WHERE username='$username' OR email= '$email'";
+            $user_check_result = mysqli_query($connection, $user_check_query);
+            if(mysqli_num_rows($user_check_result) > 0){
+                $_SESSION["signup"] = "El usuario o email ya existen";
+            } else {
+                // Trabajamos con el avatar
+                // Renombramos el avatar
+                $time = time(); // Hacemos que cada imagen tenga un nombre distinto y unico usando la fecha actual
+                $avatar_name = $time . $avatar["name"];
+                $avatar_tmp_name= $avatar["tmp_name"];
+                $avatar_destination_path = "images/" . $avatar_name;
+
+                // Me aseguro de que el archivo es una imagen
+                $allowed_files = ["png", "jpg", "jpeg"];
+                $extention = explode(".", $avatar_name);
+                $extention = end($extention);
+                if (in_array($extention, $allowed_files)){
+                    // Nos aseguramos de que la imagen so sea pesada
+                    if($avatar["size"] < 1000000){
+                        move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
+                    } else {
+                        $_SESSION["signup"] = "El archivo es demasiado grande";
+                    }
+                } else {
+                    $_SESSION["signup"] = "El archivo tiene que ser png, jpg o jpeg";
+                }
+            }
+        } 
     }
+    
 } else {
     // Si no se hizo click en el boton y se intenta acceder a la logica, redirige a la pagina de registro
     header("location: " . ROOT_URL . "signup.php");
