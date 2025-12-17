256a49c053a14cb9d25dff44792a1674a90a9bed
Pablo <pablogroza2000@gmail.com>
2025-12-17 14:22:58 +0100

    edit-post completado, consultamos a la base de datos los datos de la publicacion a traves del id y cuando enviamos el formulario enviamos a la base de datos los cambios

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -1,30 +1,45 @@
 <?php
 include "partials/header.php";
+
+// Conseguimos las categorias de la base de datos
+$category_query = "SELECT * FROM categories";
+$categories = mysqli_query($connection, $category_query);
+
+// Obtenemos la data de la peticion post de la base de datos a traves del id
+if(isset($_GET["id"])) {
+    $id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);
+    $query = "SELECT * FROM posts WHERE id=?";
+    $stmt = mysqli_prepare($connection, $query);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    $query_result = mysqli_stmt_get_result($stmt);
+    $post = mysqli_fetch_assoc($query_result);
+} else {
+    header("location: " . ROOT_URL . "admin/");
+    die();
+}
 ?>
 
 <section class="form__section-add">
     <div class="container form__section-container-add">
         <h2>Editar Publicacion</h2>
-        <form action="" enctype="multipart/form-data" method="POST">
-            <input type="text" placeholder="Titulo">
-            <select name="" id="">
-                <option value="1">Dragon Ball</option>
-                <option value="2">One Piece</option>
-                <option value="3">Demon Slayer</option>
-                <option value="4">Hunter x Hunter</option>
-                <option value="5">Jujustu Kaisen</option>
-                <option value="6">Naruto</option>
+        <form action="<?= ROOT_URL ?>admin/edit-post-logic.php" enctype="multipart/form-data" method="POST">
+            <input type="text" value="<?= $post["title"] ?>" name="title" placeholder="Titulo">
+            <select name="category" id="">
+                <?php while ($category = mysqli_fetch_assoc($categories)): ?>
+                <option value="<?= $category["id"] ?>"><?= $category["title"] ?></option>
+                <?php endwhile ?>
             </select>
-            <textarea rows="10" placeholder="Cuerpo"></textarea>
+            <textarea rows="10" placeholder="Cuerpo"><?= $post["body"] ?></textarea>
             <div class="form__control checkbox-row">
-                <input type="checkbox" id="is_featured" checked>
+                <input type="checkbox" id="is_featured" name="is_featured" value="1" checked>
                 <label for="is_featured">Destacado</label>
             </div>
             <div class="form__control">
                 <label for="thumbnail">Actualizar Imagen</label>
-                <input type="file" id="thumbnail">
+                <input type="file" name="thumbnail" id="thumbnail">
             </div>
-            <button type="submit" class="btn">Actualizar</button>
+            <button type="submit" name="submit" class="btn">Actualizar</button>
         </form>
     </div>
 </section>
